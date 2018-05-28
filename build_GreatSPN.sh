8ed22073380158def2d7791dfdc398515da11866
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-28 17:37:50 +0200

    refining the build script : meddly seems ok

--- build_GreatSPN.sh
+++ build_GreatSPN.sh
@@ -4,27 +4,38 @@ set -x
 
 
 # an IDIR = install folder
-mkdir usr
-cd usr
-mkdir local
-cd local
-export IDIR=$(pwd)
-cd ../..
+if [ ! -d usr/local ]; 
+then
+	mkdir usr
+	cd usr
+	mkdir local
+	cd local
+	export IDIR=$(pwd)
+	cd ../..
+fi
 
 
-mkdir GreatSPN
-cd GreatSPN
+if [ ! -d GreatSPN ]; 
+then
+	mkdir GreatSPN
+	cd GreatSPN
+fi
 
-git clone --depth 1 https://github.com/asminer/meddly.git --branch master --single-branch meddly
-cd meddly
-./autogen.sh
-./configure --prefix=$IDIR  || cat config.log
-make && make install
-cd ..
+if [ ! -f usr/local/include/meddly.h ];
+then
+	git clone --depth 1 https://github.com/asminer/meddly.git --branch master --single-branch meddly
+	cd meddly
+	./autogen.sh
+	./configure --prefix=$IDIR  || cat config.log
+	make && make install
+	cd ..
+fi
+
 
 
 git clone --depth 1 https://github.com/GreatSPN/SOURCES.git --branch master --single-branch SOURCES/
 cd SOURCES
+cp -f ../../patches/Makefile .
 make
 
 
