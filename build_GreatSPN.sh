81af3e4550b90fa98ff2cdf75d3fc045388dd77c
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-28 20:38:42 +0200

    build lp-solve from source

--- build_GreatSPN.sh
+++ build_GreatSPN.sh
@@ -32,12 +32,23 @@ then
 	cd ..
 fi
 
+if [ ! -d $IDIR/include/lpsolve ];
+then
+	tar xzf ../lp_solve_5.5.2.5_source.tar.gz 
+	cd lp_solve_5.5/lpsolve55
+	sh ccc
+	mkdir -p $IDIR/bin ; cp bin/ux64/liblpsolve55.* $IDIR/bin/	
+	cd ..
+	mkdir -p $IDIR/include/lpsolve ; cp *.h $IDIR/include/lpsolve/	
+	cd ..
+fi
+
 
 git clone --depth 1 https://github.com/GreatSPN/SOURCES.git --branch master --single-branch SOURCES/
 cd SOURCES
 cp -f ../../patches/Makefile .
-export CFLAGS="-O2 -Wall -Wno-unused-variables -I$IDIR/include"
-export CPPFLAGS="-O2 -Wall -Wno-unused-variables -I$IDIR/include"
+export CFLAGS="-O2 -Wall -Wno-unused-variable -I$IDIR/include"
+export CPPFLAGS="-O2 -Wall -Wno-unused-variable -I$IDIR/include"
 export LDFLAGS="-O2 -L$IDIR/lib"
 make
 
