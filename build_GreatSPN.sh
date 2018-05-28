d1f47523d982a4820d19937388b6a49829728953
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-28 21:09:23 +0200

    don't complain about dead code

--- build_GreatSPN.sh
+++ build_GreatSPN.sh
@@ -47,8 +47,8 @@ fi
 git clone --depth 1 https://github.com/GreatSPN/SOURCES.git --branch master --single-branch SOURCES/
 cd SOURCES
 cp -f ../../patches/Makefile .
-export CFLAGS="-O2 -Wall -Wno-unused-variable -I$IDIR/include"
-export CPPFLAGS="-O2 -Wall -Wno-unused-variable -I$IDIR/include"
+export CFLAGS="-O2 -Wall -Wno-unused-variable -Wno-unused-function -I$IDIR/include"
+export CPPFLAGS="-O2 -Wall -Wno-unused-variable -Wno-unused-function -I$IDIR/include"
 export LDFLAGS="-O2 -L$IDIR/lib"
 make
 
